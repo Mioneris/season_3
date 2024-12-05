09cd1d0acbd828f79d16d132d76cb6a54f869dfc
Mioneris <rumailsuck@gmail.com>
2024-12-05 18:53:02 +0600

    lesson

---
+++ season_3_lesson_1/handlers/other_mess.py
@@ -0,0 +1,10 @@
+from aiogram import Router , types
+# from aiogram.filters import Command
+# from season_3_lesson_1.main import bot
+
+echo_router = Router()
+@echo_router.message()
+async def echo_handler(message: types.Message):
+    txt = message.text
+    await message.bot.send_message(chat_id=message.from_user.id, text=txt)
+    # await message.answer(txt)
--- season_3_lesson_1/main.py
+++ season_3_lesson_1/main.py
@@ -1,25 +1,54 @@
 import asyncio
-from aiogram import Bot, Dispatcher, types
-from dotenv import dotenv_values
-from aiogram.filters import Command
+import logging
 
 
-token = dotenv_values(".env")["BOT_TOKEN"]
-bot = Bot(token=token)
-dp = Dispatcher()
 
-@dp.message(Command("start"))
-async def start_handler(message: types.Message):
-    name = message.from_user.first_name
-    await message.answer(f'Привет, {name}')
+from bot_config import dp,bot
 
-@dp.message()
-async def echo_handler(message: types.Message):
-    txt = message.text
-    await message.answer(txt)
+from handlers.start import start_router
+from handlers.picture import picture_router
+from handlers.other_mess import echo_router
+
 
 async def main():
-    # запуск бота
+    dp.include_routers(start_router,
+                       picture_router,
+                       echo_router)
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
+
+
+# @dp.message(Command("start"))
+# async def start_handler(message: types.Message):
+#     name = message.from_user.first_name
+#     # await message.answer(f'Привет, {name}')
+#     await message.reply (f"Hello {name}")
+
+
+# @dp.message(Command("picture"))
+# async def picture_handler(message: types.Message):
+#     photo = types.FSInputFile('picture/maxresdefault.jpg')
+#     await message.answer_photo(
+#         photo=photo,
+#         caption='Alphabet'
+#     )
+
+
+
+
+# @dp.message()
+# async def echo_handler(message: types.Message):
+#     txt = message.text
+#     await bot.send_message(chat_id=message.from_user.id, text=txt)
+#     # await message.answer(txt)
+
+async def main():
+    dp.include_routers(start_router,
+                       picture_router,
+                       echo_router)
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
